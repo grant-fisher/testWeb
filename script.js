4cbfa068f8ada069cf7a05a89283689483e52e26
Grant Fisher <gfisher7@stanford.edu>
2020-05-28 18:40:40 -0600

    added odometer and dynamic

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("navbar").style.padding = "30px 40px";
     document.getElementById("logo").style.fontSize = "25px";
     document.getElementById("navbar").style.backgroundColor = "rgba(255, 255, 255, 0.5)";
   } else {
-    document.getElementById("navbar").style.padding = "40px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("navbar").style.padding = "30px 40px";
+    document.getElementById("logo").style.fontSize = "40px";
     document.getElementById("navbar").style.backgroundColor = "rgba(0, 0, 0, 0)";
   }
 }
